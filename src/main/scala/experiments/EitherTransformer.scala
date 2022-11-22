17391dc3a7a9cd0bfffaac8c549bc172e18a0eda
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-22 13:11:22 +0400

    add EitherT.fromEither examples

---
+++ src/main/scala/experiments/EitherTransformer.scala
@@ -0,0 +1,38 @@
+package experiments
+
+object EitherTransformer extends scala.App {
+
+  import cats.data.EitherT
+  import cats.instances.option._
+
+  sealed trait AppError
+
+  case object Unrecoverable extends AppError
+  case object Recoverable   extends AppError
+
+  val recoverableError: EitherT[Option, String, Int] = EitherT.fromEither[Option]
+    .apply[AppError, Int](Left(Recoverable))
+    .leftSemiflatMap {
+      case Unrecoverable => None
+      case Recoverable   => Some("42")
+    }
+
+  val unrecoverableError: EitherT[Option, String, Int] = EitherT.fromEither[Option]
+    .apply[AppError, Int](Left(Unrecoverable))
+    .leftSemiflatMap {
+      case Unrecoverable => None
+      case Recoverable   => Some("55")
+    }
+
+  val successValue: EitherT[Option, String, Int] = EitherT.fromEither[Option]
+    .apply[AppError, Int](Right(10_000))
+    .leftSemiflatMap {
+      case Unrecoverable => None
+      case Recoverable   => Some("100")
+    }
+
+  println(recoverableError.value)
+  println(unrecoverableError.value)
+  println(successValue.value)
+
+}
