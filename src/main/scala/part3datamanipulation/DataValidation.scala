0a6fde1ed70109b6f3cc9edfd205272e032488b1
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-22 18:10:43 +0400

    test testNumber method using Either

--- src/main/scala/part3datamanipulation/DataValidation.scala
+++ src/main/scala/part3datamanipulation/DataValidation.scala
@@ -11,7 +11,26 @@ object DataValidation {
 
   // TODO: user Either for following example
 
+  /**
+   * - must be positive
+   * - less than 100
+   * - must be even
+   * - must not equal to 50
+   */
+  def testNumber(n: Int): Either[List[String], Int] = {
+    val isPositive: List[String] = if (n > 0) Nil else List("It is non-positive")
+    val isLessThan100: List[String] = if (n < 100) Nil else List("Is 100 or more")
+    val isEven: List[String] = if (n % 2 == 0) Nil else List("Is odd")
+    val isNotEqualTo50: List[String] = if (n != 50) Nil else List("it's 50")
+
+    if (isPositive.nonEmpty || isLessThan100.nonEmpty || isEven.nonEmpty || isNotEqualTo50.nonEmpty) Left(isPositive ::: isLessThan100 ::: isEven ::: isNotEqualTo50)
+    else Right(n)
+  }
+
+
   def main(args: Array[String]): Unit = {
+    println(testNumber(4))
+    println(testNumber(5))
 
   }
 
