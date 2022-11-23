bd27073dff6b7eb8bcf7230ccb5c481d81dee363
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-23 12:55:12 +0400

    finish first exercise for Semigroupals

--- src/main/scala/part4typeclasses/Semigroupals.scala
+++ src/main/scala/part4typeclasses/Semigroupals.scala
@@ -25,7 +25,30 @@ object Semigroupals {
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
     val aTupledFuture = Semigroupal[Future].product(Future("meaning of life"), Future(42)) // Future(("meaning of life", 42))
 
+    import cats.instances.list._
 
+    val aTupledList = Semigroupal[List].product(1 :: 2 :: Nil, "a" :: "b" :: Nil)
+    println(aTupledList)
+    println {
+      for {
+        a <- 1 :: 2 :: Nil
+        b <- "a" :: "b" :: Nil
+      } yield (a, b)
+    }
+
+    // TODO
+    import cats.Monad
+    import cats.syntax.flatMap._
+    import cats.syntax.functor._
+
+    def productWithMonads[F[_]: Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+      for {
+        a <- fa
+        b <- fb
+      } yield (a, b)
+
+    def productWithMonadsUgly[F[_]: Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+      Monad[F].flatMap(fa)(a => Monad[F].map(fb)(b => (a, b)))
 
   }
 
