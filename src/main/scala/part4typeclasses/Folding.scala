677867fd1f65433fee973bfed20b2533682d84e7
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-25 11:11:26 +0400

    write definitions

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -3,7 +3,8 @@ package part4typeclasses
 object Folding {
 
   object list_exercises {
-
+    def map[A, B](list: List[A])(f: A => B): List[B] = list.foldLeft()
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = ???
   }
 
 }
