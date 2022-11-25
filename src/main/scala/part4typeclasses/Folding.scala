07f830b19b9cd7619522250939ac0d015baa7078
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-25 11:17:12 +0400

    implement map & flatMap using foldRight on lists

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -6,10 +6,16 @@ import cats.Monoid
 object Folding {
 
   object list_exercises {
-    def map[A, B](list: List[A])(f: A => B): List[B] = ???
-    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = ???
+    def map[A, B](list: List[A])(f: A => B): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) :: acc)
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) ::: acc)
     def filter[A](list: List[A])(f: A => Boolean): List[A] = ???
-    def combine[A: Monoid](list: List[A]): A = ???
+    def combine[A: Monoid](list: List[A]): A = list.foldLeft(Monoid[A].empty)(Monoid[A].combine)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    import list_exercises._
+
   }
 
 }
