69c488168aefdb78fd311372ffd9246955d4ec8b
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-25 11:20:34 +0400

    test flatMap

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -7,7 +7,7 @@ object Folding {
 
   object list_exercises {
     def map[A, B](list: List[A])(f: A => B): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) :: acc)
-    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) ::: acc)
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) ++ acc)
     def filter[A](list: List[A])(f: A => Boolean): List[A] = ???
     def combine[A: Monoid](list: List[A]): A = list.foldLeft(Monoid[A].empty)(Monoid[A].combine)
   }
@@ -18,6 +18,10 @@ object Folding {
 
     val data = 1 :: 2 :: 3 :: Nil
     assert(map(data)(_ * 2) == 2 :: 4 :: 6 :: Nil)
+
+    assert(flatMap(data)(n => List(n * 2)) == 2 :: 4 :: 6 :: Nil)
+
+
 
   }
 
