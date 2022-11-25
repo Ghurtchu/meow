3172f71cb90b3d4f863055f7ac5df816e1df51e8
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-25 11:30:03 +0400

    implement filter by foldRight

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -8,7 +8,7 @@ object Folding {
   object list_exercises {
     def map[A, B](list: List[A])(f: A => B): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) :: acc)
     def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list.foldRight(List.empty[B])((elem, acc) => f(elem) ++ acc)
-    def filter[A](list: List[A])(f: A => Boolean): List[A] = ???
+    def filter[A](list: List[A])(f: A => Boolean): List[A] = list.foldRight(List.empty[A])((a, curr) => if (f(a)) a :: curr else curr)
     def combine[A: Monoid](list: List[A]): A = list.foldLeft(Monoid[A].empty)(Monoid[A].combine)
   }
 
@@ -20,6 +20,7 @@ object Folding {
     assert(map(data)(_ * 2) == 2 :: 4 :: 6 :: Nil)
 
     assert(flatMap(data)(n => List(n * 2)) == 2 :: 4 :: 6 :: Nil)
+
 
 
 
