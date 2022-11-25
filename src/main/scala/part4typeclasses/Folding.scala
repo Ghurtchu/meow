2dfcd21e06fe2977e14d2fa352f8ef5e4074ef6a
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-25 11:12:32 +0400

    add definitions for filter and combine

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -1,10 +1,15 @@
 package part4typeclasses
 
+import cats.Monoid
+
+
 object Folding {
 
   object list_exercises {
-    def map[A, B](list: List[A])(f: A => B): List[B] = list.foldLeft()
+    def map[A, B](list: List[A])(f: A => B): List[B] = ???
     def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = ???
+    def filter[A](list: List[A])(f: A => Boolean): List[A] = ???
+    def combine[A: Monoid](list: List[A]): A = ???
   }
 
 }
