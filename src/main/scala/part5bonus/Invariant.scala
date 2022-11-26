b39c53a14552cc530352f810c556bcfcb66a1442
nghurtch <anzori.gurchumelia@gau.edu.ge>
2022-11-26 17:48:46 +0400

    implement CaesarCypher for strings and test them

--- src/main/scala/part5bonus/Invariant.scala
+++ src/main/scala/part5bonus/Invariant.scala
@@ -2,10 +2,25 @@ package part5bonus
 
 object Invariant {
 
+  trait Crypto[A] {
+    def encrypt(a: A): String // from -> to
+    def decrypt(s: String): A // to   -> from
+  }
 
+  def encrypt[A](a: A)(implicit crypto: Crypto[A]): String = crypto encrypt a
+  def decrypt[A](s: String)(implicit crypto: Crypto[A]): A = crypto decrypt s
+
+  implicit val caesarCypher: Crypto[String] = new Crypto[String] {
+    override def encrypt(a: String): String = a.map(c => (c + 2).toChar)
+    override def decrypt(s: String): String = s.map(c => (c - 2).toChar)
+  }
 
   def main(args: Array[String]): Unit = {
+    val encrypted = encrypt("Let's encrypt")
+    val decrypted = decrypt(encrypted)
 
+    println(encrypted)
+    println(decrypted)
   }
 
 }
